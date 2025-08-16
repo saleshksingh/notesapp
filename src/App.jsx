9deffdfdc8eb1ab1d9c7c96af4d7a1715ecf7375
Salesh Singh <salsinx@amazon.com>
2025-08-16 11:54:20 -0700

    changes for  notesapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-function App() {
+import React, { useEffect, useState } from "react";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import amplifyConfig from "../amplify_outputs.json"; // or aws-exports.js
+
+Amplify.configure(amplifyConfig);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-        <h1>Hello from Amplify</h1>
-      </header>
+    <div>
+      <h1>Hello, {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+      {/* Additional UI flows (notes, images) go here */}
     </div>
   );
 }
-export default App;
+
+export default function AppWithAuth() {
+  return <Authenticator><App /></Authenticator>;
+}
